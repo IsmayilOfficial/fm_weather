11b18bb958097fcd4846f2a1d85c2a0a277dbfd7
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-12 20:30:02 +0400

    init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+
+  const [location, setLocation] = useState<undefined | Location.LocationObject>(undefined);
+
+  async function getLocation() {
+    const {status} = await Location.requestPermissionsAsync();
+    if(status !== 'granted'){
+      alert('location permission denied. App will not work.');
+    }
+   const userLocation = await Location.getCurrentPositionAsync();
+   setLocation(userLocation);
+    
+  }
+  useEffect(() => {
+    getLocation();
+   
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This will be my first weather app</Text>
+      <Text>{location?.coords.latitude}</Text>
+      <Text>{location?.coords.longitude}</Text>
       <StatusBar style="auto" />
     </View>
   );
