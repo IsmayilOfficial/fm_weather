a38c12decd31fcdf16b720e7b6a3e202e014fc95
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-12 21:46:58 +0400

    simple view

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect,useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 
-
+const API_KEY = "1a9c835c8268dfc1c2edb8f99313a945";
 export default function App() {
 
   const [location, setLocation] = useState<undefined | Location.LocationObject>(undefined);
+  const [currentWeather, setCurrentWeather] = useState<undefined | any>(undefined)
+  const [weatherIconLink, setWeatherIconLink] = useState<undefined | string>(undefined)
+
 
   async function getLocation() {
     const {status} = await Location.requestPermissionsAsync();
@@ -21,11 +24,30 @@ export default function App() {
     getLocation();
    
   }, [])
+
+  async function getWeather() {
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location?.coords.latitude}&lon=${location?.coords.longitude}&exclude={part}&appid=${API_KEY}&units=metric`)
+    const weatherResponse = await response.json();
+    const {current} = weatherResponse;
+    setCurrentWeather(current); 
+    // const weatherLink = `http://openweathermap.org/img/wn/10d@2x.png`;
+    const weatherLink = `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`;
+    setWeatherIconLink(weatherLink);
+
+    console.log(current);
+
+  }
+  useEffect(()=>{
+    getWeather();
+  },[location])
   return (
     <View style={styles.container}>
-      <Text>This will be my first weather app</Text>
-      <Text>{location?.coords.latitude}</Text>
-      <Text>{location?.coords.longitude}</Text>
+      
+      <Text style={{fontSize: 50}}>{ currentWeather?.temp + "°C"} </Text>
+      <Image style = {{width: 300, height: 300}} source ={{uri: weatherIconLink }}></Image>
+
+    
       <StatusBar style="auto" />
     </View>
   );
